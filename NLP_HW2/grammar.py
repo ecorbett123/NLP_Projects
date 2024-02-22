aecd18df4551317c6ea628cef41a6dc6be0a5fe9
Emma Corbett <40502169+ecorbett123@users.noreply.github.com>
2024-02-21 22:18:18 -0500

    part 1 done

--- NLP_HW2/grammar.py
+++ NLP_HW2/grammar.py
@@ -3,7 +3,7 @@ COMS W4705 - Natural Language Processing - Fall 2023
 Homework 2 - Parsing with Context Free Grammars 
 Daniel Bauer
 """
-
+import math
 import sys
 from collections import defaultdict
 from math import fsum
@@ -47,8 +47,31 @@ class Pcfg(object):
         Return True if the grammar is a valid PCFG in CNF.
         Otherwise return False. 
         """
-        # TODO, Part 1
-        return False 
+        # First check that all the probabilities sum to one
+        for entry in self.lhs_to_rules:
+            total_probability = 0
+            if len(self.lhs_to_rules[entry]) < 1:
+                return False # no values provided for a token (probability not summing to one)
+            for rule in self.lhs_to_rules[entry]:
+                if len(rule) == 3:
+                    total_probability += rule[2]
+                else:
+                    return False # rule not in correct format
+            if not math.isclose(total_probability, 1.0):
+                return False # probability for entry not summing to one
+
+        # Next check that each rule is in Chomsky Normal form
+        nonterminals = self.lhs_to_rules.keys()
+        for entry in self.rhs_to_rules:
+            if len(entry) != 2 and len(entry) != 1:
+                return False # not two non-terminals or one terminal
+            if len(entry) == 2:
+                if entry[0] not in nonterminals or entry[1] not in nonterminals:
+                    return False # should be tuple of two non-terminals
+            if len(entry) == 1:
+                if entry[0] in nonterminals:
+                    return False # if length is one, should be just a terminal
+        return True
 
 
 if __name__ == "__main__":
