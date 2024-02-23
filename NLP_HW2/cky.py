178cf96bc439d77d02ff8ac601f13cc56b092526
Emma Corbett <40502169+ecorbett123@users.noreply.github.com>
2024-02-22 21:35:36 -0500

    lmao and we are done

--- NLP_HW2/cky.py
+++ NLP_HW2/cky.py
@@ -115,7 +115,7 @@ class CkyParser(object):
                         b_nonterminals = parse_table[(k, j)]
                         for a_nonterminal in a_nonterminals:
                             for b_nonterminal in b_nonterminals:
-                                if (a_nonterminal[0], b_nonterminal[0]) in grammar.rhs_to_rules:
+                                if (a_nonterminal[0], b_nonterminal[0]) in self.grammar.rhs_to_rules:
                                     replace_list.extend(grammar.rhs_to_rules[(a_nonterminal[0], b_nonterminal[0])])
                         if (i, j) not in parse_table and len(replace_list) > 0:
                             parse_table[i, j] = replace_list
@@ -137,8 +137,9 @@ class CkyParser(object):
         num_tokens = len(tokens)
         # initialization
         for i in range(num_tokens):
-            if (tokens[i],) not in self.grammar.rhs_to_rules:
-                return None, None  # terminal not in set of terminals
+            # Comment this out to preserve formatting...
+            # if (tokens[i],) not in self.grammar.rhs_to_rules:
+            #     return None, None  # terminal not in set of terminals
 
             table_map = {}
             prob_map = {}
@@ -163,9 +164,9 @@ class CkyParser(object):
                         b_nonterminals = table[(k, j)]
                         for a_nonterminal in a_nonterminals:
                             for b_nonterminal in b_nonterminals:
-                                if (a_nonterminal, b_nonterminal) in grammar.rhs_to_rules:
+                                if (a_nonterminal, b_nonterminal) in self.grammar.rhs_to_rules:
                                     # for each of the rules, add them to map of greater probability
-                                    for nonterminal in grammar.rhs_to_rules[(a_nonterminal, b_nonterminal)]:
+                                    for nonterminal in self.grammar.rhs_to_rules[(a_nonterminal, b_nonterminal)]:
                                         if nonterminal[0] not in table_map or math.log2(nonterminal[2]) + probs[(i,k)][a_nonterminal] + probs[(k,j)][b_nonterminal] > prob_map[nonterminal[0]]:
                                             table_map[nonterminal[0]] = ((a_nonterminal, i, k), (b_nonterminal, k, j))
                                             combined_prob = math.log2(nonterminal[2]) + probs[(i,k)][a_nonterminal] + probs[(k,j)][b_nonterminal]
@@ -183,13 +184,17 @@ def get_tree(chart, i,j,nt):
     Return the parse-tree rooted in non-terminal nt and covering span i,j.
     """
     if (i,j) not in chart or nt not in chart[(i,j)]:
-        return None
+        raise KeyError # throw a key error to get correct coverage score (happens when index not in char or token not in index of char)
     if isinstance(chart[(i,j)][nt], str):
         return (nt, chart[(i,j)][nt])
 
     split1 = chart[(i,j)][nt][0]
     split2 = chart[(i, j)][nt][1]
-    return (nt, get_tree(chart, split1[1], split1[2], split1[0]), get_tree(chart, split2[1], split2[2], split2[0]))
+    left = get_tree(chart, split1[1], split1[2], split1[0])
+    right = get_tree(chart, split2[1], split2[2], split2[0])
+    if not left or not right:
+        return None
+    return (nt, left, right)
  
        
 if __name__ == "__main__":
@@ -198,7 +203,6 @@ if __name__ == "__main__":
         grammar = Pcfg(grammar_file) 
         parser = CkyParser(grammar)
         toks =['flights', 'from','miami', 'to', 'cleveland','.']
-        #toks = ['she', 'saw', 'the', 'cat', 'with', 'glasses']
         print(parser.is_in_language(toks))
         table,probs = parser.parse_with_backpointers(toks)
         assert check_table_format(table)
